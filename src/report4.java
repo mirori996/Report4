d2458b0d5b87ed777cfa246420fe905d1041f7f0
Tamahashi Yutaro <e175743@ie.u-ryukyu.ac.jp>
2017-11-27 18:52:22 +0900

    NullPointerExceptionをcatchし、NullPointerException、getMessage()を出力。

--- src/report4.java
+++ src/report4.java
@@ -1,7 +1,12 @@
 public class report4 {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e) {
+            System.out.println("NullPointerExceptionが出てるよ");
+            System.out.println(e.getMessage());
         }
     }
+}
 
